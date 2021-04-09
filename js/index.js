747aff9e13909724fa03cc957c3e2951692acb1b
Tanya Kalkuta <Tanyakalkuta@gmail.com>
2021-04-09 22:46:03 +0300

    таймер обратного отсчета

--- js/index.js
+++ js/index.js
@@ -1,35 +1,75 @@
-// new CountdownTimer({
-//   selector: '#timer-1',
-//     targetDate: new Date('Jul 04, 2021'),
-//        
-// });
+import refs from './refs.js'
 
 
+class CountdownTimer{
+  constructor({onTick}, date) {
+    this.intervalId = null;
+    this.isActive = false;
+    this.onTick = onTick;
+    this.date = date;
+    this.init();
+  }
 
-const timer = {
-  start() {    
-    const targetDate = new Date('Jul 04, 2021');
+  init() {
+    const time = this.getTimeComponents(0);
+    this.onTick(time);
+  }
 
-    setInterval(() => {
+  start() {
+    if (this.isActive) {
+      return;
+    }
+
+    const targetDate = new Date(this.date);
+    this.isActive = true;
+
+    this.intervalId = setInterval(() => {
       const currentTime = Date.now()
       const deltaTime = targetDate - currentTime;
-      const { days, hours, mins, secs } = getTimeComponents(deltaTime);
-      console.log(`${days}::${hours}::${mins}::${secs}`);
+      const time = this.getTimeComponents(deltaTime);
+     // updateTimer(time);
+      this.onTick(time);
+      console.log(time);
     }, 1000);
   }
-};
 
-// timer.start();
+  
+  stop() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+    const time = this.getTimeComponents(0);
+    this.onTick(time);
+  }
 
-function pad(value) {
-  return String(value).padStart(2, '0');
+  getTimeComponents(time) {
+  const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+  const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    return { days, hours, mins, secs };
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+
 }
 
-function getTimeComponents(time) {
-  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-  const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-  return { days, hours, mins, secs };
-}
 
+const timer = new CountdownTimer({
+  onTick: updateTimer,
+}, 'Jul 04 2021');
+
+
+refs.startBtn.addEventListener('click', timer.start.bind(timer));
+
+refs.stoptBtn.addEventListener('click', timer.stop.bind(timer));
+
+
+
+function updateTimer({days, hours, mins, secs }) {
+    refs.days.textContent = days;
+    refs.hours.textContent = hours;
+    refs.mins.textContent = mins;
+    refs.secs.textContent = secs;
+}
